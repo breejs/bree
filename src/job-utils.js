baedefc81a5d733279c38b55f880d6d51a31b33c
Naz <nazargargol@gmail.com>
2020-12-01 13:12:56 +1300

    docs: added jsdoc descriptions to job utils module

--- src/job-utils.js
+++ src/job-utils.js
@@ -3,22 +3,45 @@ const humanInterval = require('human-interval');
 const later = require('@breejs/later');
 const isSANB = require('is-string-and-not-blank');
 
+/**
+ * Naively checks if passed value is of later.js schedule format (https://breejs.github.io/later/schedules.html)
+ *
+ * @param {*} value to check for schedule format
+ * @returns {boolean}
+ */
 const isSchedule = (value) => {
   return typeof value === 'object' && Array.isArray(value.schedules);
 };
 
+/**
+ * Extracts job name from job definition
+ *
+ * @param {string | Object | Function} job definition
+ * @returns {string}
+ */
 const getName = (job) => {
   if (isSANB(job)) return job;
   if (typeof job === 'object' && isSANB(job.name)) return job.name;
   if (typeof job === 'function' && isSANB(job.name)) return job.name;
 };
 
+/**
+ * Parses provided value into millisecond
+ *
+ * @param {string} _value
+ */
 const getHumanToMs = (_value) => {
   const value = humanInterval(_value);
   if (Number.isNaN(value)) return ms(_value);
   return value;
 };
 
+/**
+ * Parses schedule value into "later" schedule object or milliseconds
+ *
+ * @param {boolean | string | number | Object} value
+ * @returns {number | boolean | Object}
+ */
 const parseValue = (value) => {
   if (value === false) return value;
 
@@ -38,6 +61,14 @@ const parseValue = (value) => {
   return value;
 };
 
+/**
+ * Processes job objects extracting their names
+ * Can conditionaly skip records by their index
+ *
+ * @param {any[]} jobs
+ * @param {number} excludeIndex
+ * @returns {string[]} job names
+ */
 const getJobNames = (jobs, excludeIndex) => {
   const names = [];
 
