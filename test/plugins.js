aa872be7533ab0bad84f6355e6419ba8644ae942
Taylor Schley <taylorschley@me.com>
2021-07-28 09:43:17 -0500

    test: add plugin tests

---
+++ test/plugins.js
@@ -0,0 +1,79 @@
+const test = require('ava');
+const path = require('path');
+
+const Bree = require('../src');
+
+const root = path.join(__dirname, 'jobs');
+const baseConfig = {
+  root,
+  timeout: 0,
+  interval: 0,
+  hasSeconds: false,
+  defaultExtension: 'js'
+};
+
+test('successfully add plugin', (t) => {
+  t.plan(2);
+
+  const plugin = (_, c) => {
+    c.prototype.test = () => {
+      t.pass();
+    };
+  };
+
+  Bree.extend(plugin);
+
+  t.is(plugin.$i, true);
+
+  const bree = new Bree({ ...baseConfig });
+
+  bree.test();
+});
+
+test('successfully add plugin with options', (t) => {
+  t.plan(1);
+
+  const plugin = (options, c) => {
+    c.prototype.test = () => {
+      if (options.test) {
+        t.pass();
+      } else {
+        t.fail();
+      }
+    };
+  };
+
+  Bree.extend(plugin, { test: true });
+
+  const bree = new Bree({ ...baseConfig });
+
+  bree.test();
+});
+
+test('only adds plugin once', (t) => {
+  t.plan(2);
+
+  let count = 0;
+
+  const plugin = (_, c) => {
+    if (count === 1) {
+      t.fail();
+    }
+
+    count++;
+
+    c.prototype.test = () => {
+      t.pass();
+    };
+  };
+
+  Bree.extend(plugin);
+
+  t.is(plugin.$i, true);
+
+  Bree.extend(plugin);
+
+  const bree = new Bree({ ...baseConfig });
+
+  bree.test();
+});
